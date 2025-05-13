6fbf8a3ca80e17e2019f3f5f78c2cf32b89b0a53
Aung Myint Myat <amm926616@gmail.com>
2025-05-13 16:00:47 +0630

    updated

--- src/components/SessionComponent.tsx
+++ src/components/SessionComponent.tsx
@@ -7,13 +7,18 @@ interface SectionProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
 }
 
-const Section: React.FC<SectionProps> = ({ id, children, className, ...rest }) => {
+const Section: React.FC<SectionProps> = ({
+  id,
+  children,
+  className,
+  ...rest
+}) => {
   return (
     <section
       id={id}
       className={twMerge(
         "scroll-mt-30 py-24 px-4 sm:px-8 md:px-16 lg:px-32 w-full max-w-7xl mx-auto",
-        className
+        className,
       )}
       {...rest}
     >
