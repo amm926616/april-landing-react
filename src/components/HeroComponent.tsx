72e7c2802f149046662b295bbd357444985de7b5
Aung Myint Myat <amm926616@gmail.com>
2025-05-13 16:13:14 +0630

    updated

--- src/components/HeroComponent.tsx
+++ src/components/HeroComponent.tsx
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
-export default function HeroComponent() {
-  return (
-    <div className="flex flex-col justify-center items-center h-[160px] w-full bg-slate-500 my-2">
-      <div className="text-red-500 text-xl font-bold font-caskaydia mb-2">
-        Hero Component
-      </div>
-      <div className="btn-primary cursor-pointer">A new Button</div>
-    </div>
-  );
-=======
 import { motion } from "framer-motion";
 import { useEffect, useRef, useState, useMemo } from "react";
 import aprilHeroIcon from "/src/assets/images/april-icon.png";
 
 export interface HeroProps {
   scrollToFeatures: () => void;
->>>>>>> update/framer-motion
 }
 
 const Hero: React.FC<HeroProps> = ({ scrollToFeatures }) => {
